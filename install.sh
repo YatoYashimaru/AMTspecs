a4692a0e4e99cdcc7848dc208b4b3394d05646c7
YatoYashimaru <lehoangthien2007@gmail.com>
2026-01-29 20:23:49 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Move the files
-sudo cp bin/AMTsp /usr/local/bin/AMTspecs
+sudo cp bin/AMTsp /usr/local/bin
 sudo cp -r Images /usr/local/share
 
 # Make the script executable
-sudo chmod +x /usr/local/bin/AMTspecs/AMTsp
+sudo chmod +x /usr/local/bin/AMTsp
 
 echo "Installation complete"
